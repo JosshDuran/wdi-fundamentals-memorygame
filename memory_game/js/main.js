7fd8dddff0b194324d0d4edcd558c9391341c89f
Jossh Duran <josshduran@Jennifers-MacBook-Pro.local>
2019-03-10 14:14:13 -0700

    create game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,15 @@
+console.log("Up and running!");
+var cards = [ "queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push('cardOne');
+console.log("User flipped " + cardOne);
+var cardTwo = cards[2];
+cardsInPlay.push('cardTwo');
+console.log("User flipped " + cardTwo);
+if (cardsInPlay.length === 2) {
+	result = "";
+} else if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log(alert("You found a match!"));
+}; 
+console.log(alert("Sorry, try again"));
